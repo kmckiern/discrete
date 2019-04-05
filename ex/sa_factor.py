b6d66120323833a09ecf4a58a3aefb921ce20d97
kmckiern <kmckiern@stanford.edu>
2019-04-05 15:33:16 -0700

    unify problem variables to header

--- ex/sa_factor.py
+++ ex/sa_factor.py
@@ -7,13 +7,15 @@ import dwavebinarycsp as dbc
 # https://github.com/dwavesystems/factoring-demo/blob/master/factoring/interfaces.py
 
 P = 12
-num_reads = 50
+n_bits = 3
+min_classical_gap = 0.1
+n_reads = 50
 
 ### FORMULATE PROBLEM
-csp = dbc.factories.multiplication_circuit(3)
-bqm = dbc.stitch(csp, min_classical_gap=.1)
+csp = dbc.factories.multiplication_circuit(n_bits)
+bqm = dbc.stitch(csp, min_classical_gap=min_classical_gap)
 
-p_vars = ['p0', 'p1', 'p2', 'p3', 'p4', 'p5']
+p_vars = ['p{}'.format(i) for i in n_bits*2]
 fixed_variables = dict(zip(reversed(p_vars), "{:06b}".format(P)))
 fixed_variables = {var: int(x) for (var, x) in fixed_variables.items()}
 for var, value in fixed_variables.items():
@@ -21,18 +23,18 @@ for var, value in fixed_variables.items():
 
 ### RUN
 sampler = dimod.SimulatedAnnealingSampler()
-response = sampler.sample(bqm, num_reads=10)
+response = sampler.sample(bqm, num_reads=n_reads)
 
 ### RESULTS
-a_vars = ['a0', 'a1', 'a2']
-b_vars = ['b0', 'b1', 'b2']
+a_vars = ['a{}'.format(i) for i in n_bits]
+b_vars = ['b{}'.format(i) for i in n_bits]
 if 'num_occurrences' not in response.data_vectors:
     response.data_vectors['num_occurrences'] = [1] * len(response)
 total = sum(response.data_vectors['num_occurrences'])
+assert total == n_reads
 
 results_dict = OrderedDict()
 for sample, num_occurrences in response.data(['sample', 'num_occurrences']):
-    # convert A and B from binary to decimal
     a = b = 0
     for lbl in reversed(a_vars):
         a = (a << 1) | sample[lbl]
